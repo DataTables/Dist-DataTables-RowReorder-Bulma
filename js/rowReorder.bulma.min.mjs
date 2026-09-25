3dd2fbb364d3d1a49dd1f049f44c3cd816f1c178
dtbuild <dtbuild@rigil>
2026-09-25 13:32:22 +0000

    025085178766203f9389d4ba9e639703f439c0dd Release 2.1.1
    Sync to source repo @025085178766203f9389d4ba9e639703f439c0dd

--- js/rowReorder.bulma.min.mjs
+++ js/rowReorder.bulma.min.mjs
@@ -1,4 +1,4 @@
-/*! RowReorder Bulma styling 2.1.0 for DataTables
+/*! RowReorder Bulma styling 2.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bm";import RowReorder from"datatables.net-rowreorder";export default DataTable;
--- js/rowReorder.bulma.mjs
+++ js/rowReorder.bulma.mjs
@@ -1,4 +1,4 @@
-/*! RowReorder Bulma styling 2.1.0 for DataTables
+/*! RowReorder Bulma styling 2.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
